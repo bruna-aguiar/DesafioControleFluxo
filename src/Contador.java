7b6266b46786c0c7880b0171a5a6e4cf1879e570
Bruna Aguiar <baguiar861@gmail.com>
2024-12-05 19:33:55 -0300

    Programa de Contagem finalizado.

--- src/Contador.java
+++ src/Contador.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) {
-        System.out.println("Teste de Git. Ass: Bruna");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o primeiro numero: ");
+        int numero1 = scanner.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int numero2 = scanner.nextInt();
+
+        try{
+            contar(numero1,numero2);
+        } catch (ParamentrosInvalidosException e){
+            System.out.println("O numero 2 tem que ser maior que o numero 1.");
+        }
     }
+    static void contar(int numero1, int numero2) throws ParamentrosInvalidosException {
+        if (numero1>numero2){
+           throw new ParamentrosInvalidosException();
+        }
+        int contagem = numero2 - numero1;
+        System.out.println("A contagem deve ser de " + contagem);
+        for(int i = 1; i<=contagem;i++ ){
+            System.out.println(i);
+        }
+    }
+
 }
--- src/ParamentrosInvalidosException.java
+++ src/ParamentrosInvalidosException.java
@@ -1,3 +1,3 @@
-public class ParamentrosInvalidosException {
+public class ParamentrosInvalidosException extends Exception {
 
 }
